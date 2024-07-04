5aa517702d991d430b449e9f3670feabd9351207
Rlisbeth <karinalisbethmanyailvis@gmail.com>
2024-07-04 16:11:51 -0500

    first commit

---
+++ R/hello.R
@@ -0,0 +1,26 @@
+install.packages("devtools")
+library(devtools)
+
+
+elevado <- function(base,expo) {
+  c <- base^expo
+  print("la potencia es")
+  print(c)
+}
+
+dijitos <- function(Numero_3_digitos){
+  N<- Numero_3_digitos
+  if(N>=100 & N < 1000){
+    dig1<- trunc(N/100)
+    x1 <-N%% 100
+    dig2 <- trunc(x1/10)
+    dig3 <- x1%%10
+    s <- dig1+dig2+dig3
+    print("LA SUMA DE LOS DIGITOS ES :")
+    print(s)
+    else{
+      print("no es un numero de 3 digitos ")
+    }
+
+  }
+}
